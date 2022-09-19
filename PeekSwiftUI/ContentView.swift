2be11a9968437a86ca5b9137dc1a8abdc3f938c1
Saurav Kumar <t-saurav5.kumar@paytmmoney.com>
2022-09-19 13:59:44 +0530

    Initial Commit

---
+++ PeekSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PeekSwiftUI
+//
+//  Created by Saurav  Kumar on 19/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PeekSwiftUI/PeekSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeekSwiftUIApp.swift
+//  PeekSwiftUI
+//
+//  Created by Saurav  Kumar on 19/09/22.
+//
+
+import SwiftUI
+
+@main
+struct PeekSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
